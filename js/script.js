cb3781bc3b6a885a4c176bf0bc256c327057a1ab
dennis <maft.den@gmail.com>
2018-01-05 00:53:52 +0200

    response for phone and new page success

--- js/script.js
+++ js/script.js
@@ -1,106 +1,155 @@
 (function($) {
 
     // input -----------------------------------------------------------------------------------------------------------
-    $(".area__input").find("span").click(function () {
-        var calc = $('.area__input .inp');
-        var calcText = calc.val();
+    $(document).ready(function () {
+        $(".area__input").find("span").click(function () {
+            var calc = $('.area__input .inp');
+            var calcText = calc.val();
 
-        if ($(this).hasClass("minus") && calcText > 1) {
-            calc.val((+calcText - 0.1).toFixed(1));
-        } else if ($(this).hasClass("plus") && calcText >= 1) {
-            calc.val((+calcText + 0.1).toFixed(1));
-        } else if ($(this).hasClass("plus") && calcText < 1) {
-            calc.val(1);
-        }
-
+            if ($(this).hasClass("minus") && calcText > 1) {
+                calc.val((+calcText - 0.1).toFixed(1));
+            } else if ($(this).hasClass("plus") && calcText >= 1) {
+                calc.val((+calcText + 0.1).toFixed(1));
+            } else if ($(this).hasClass("plus") && calcText < 1) {
+                calc.val(1);
+            }
+        });
     });
 
     // slider ----------------------------------------------------------------------------------------------------------
-    $('.slider').slick({
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        draggable: false
-    });
-
-    var current = 1,
-        count   = $('div.slide');
-
-    $('.works .current').html(current);
-    $('.works .count').html(count.length);
-
-    $('.works .slick-prev').click(function () {
-        current === 1 ? current = count.length : current--;
-
-        $('.works .current').html(current);
-    });
-
-    $('.works .slick-next').click(function () {
-        current === count.length ? current = 1 : current++;
-
-        $('.works .current').html(current);
-    });
-
-    // slider-rates ----------------------------------------------------------------------------------------------------
-    if ($(window).width() <= 768) {
-        $('.rates-info').slick({
+    $(document).ready(function () {
+        $('.slider').slick({
             infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
             speed: 500,
             fade: true,
             cssEase: 'linear',
             draggable: false
         });
 
-        var current2 = 1,
-            count2   = 2;
 
-        $('.rates .current').html(current2);
-        $('.rates .count').html(count2);
+        var current = 1,
+            count   = $('div.slide');
 
-        $('.rates .slick-prev').click(function () {
-            current2 === 1 ? current2 = count2 : current2--;
+        $('.works .current').html(current);
+        $('.works .count').html(count.length);
 
-            $('.rates .current').html(current2);
+        $('.works .slick-prev').click(function () {
+            current === 1 ? current = count.length : current--;
+
+            $('.works .current').html(current);
         });
 
-        $('.rates .slick-next').click(function () {
-            current2 === count2 ? current2 = 1 : current2++;
+        $('.works .slick-next').click(function () {
+            current === count.length ? current = 1 : current++;
+
+            $('.works .current').html(current);
+        });
+    });
+
+    // slider-rates ----------------------------------------------------------------------------------------------------
+    $(document).ready(function () {
+        if ($(window).width() <= 768) {
+            $('.rates-info').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 500,
+                fade: true,
+                cssEase: 'linear',
+                draggable: false
+            });
+
+
+            var current2 = 1,
+                count2   = 2;
 
             $('.rates .current').html(current2);
-        });
-    }
+            $('.rates .count').html(count2);
 
+            $('.rates .slick-prev').click(function () {
+                current2 === 1 ? current2 = count2 : current2--;
+
+                $('.rates .current').html(current2);
+            });
+
+            $('.rates .slick-next').click(function () {
+                current2 === count2 ? current2 = 1 : current2++;
+
+                $('.rates .current').html(current2);
+            });
+        }
+    });
 
 
     // reviews ---------------------------------------------------------------------------------------------------------
-    if ($(window).width() <= 768) {
-        var widthItem = $(window).width() - 50,
-            totalItem = $('.review-view__item');
+    $(document).ready(function () {
+        if ($(window).width() <= 768 && $(window).width() > 320) {
+            var widthItem = $(window).width() - 50,
+                totalItem = $('.review-view__item');
 
-        totalItem.css('width', widthItem);
-        $('.review-view').css('width', (totalItem.length * widthItem) + (29 * totalItem.length));
-    } else {
-        var reviewWidth = $('.review-view__item'),
-            resultReview = (reviewWidth.length * 584) - 29;
+            totalItem.css('width', widthItem);
+            $('.review-view').css('width', (totalItem.length * widthItem) + (29 * totalItem.length));
+        } else if ($(window).width() <= 320) {
+            var widthItemPhone = $(window).width() - 30,
+                totalItemPhone = $('.review-view__item');
 
-        $('.review-view').css('width', resultReview);
-    }
+            totalItemPhone.css('width', widthItemPhone);
+            $('.review-view').css('width', (totalItemPhone.length * widthItemPhone) + (29 * totalItemPhone.length))
+        } else {
+            var reviewWidth = $('.review-view__item'),
+                resultReview = (reviewWidth.length * 584) - 29;
+
+            $('.review-view').css('width', resultReview);
+        }
+
+
+        $('.reviews-slide').niceScroll({
+            cursorcolor: "#fcc936",
+            cursoropacitymin: 1,
+            cursorwidth: "7px",
+            cursorborderradius: 0,
+            cursorfixedheight: 198,
+            enablemousewheel: false,
+            grabcursorenabled: true
+        });
 
 
 
 
-    $('.reviews-slide').niceScroll({
-        cursorcolor: "#fcc936",
-        cursoropacitymin: 1,
-        cursorwidth: "7px",
-        cursorborderradius: 0,
-        cursorfixedheight: 198,
-        enablemousewheel: false,
-        grabcursorenabled: true
+        if ($(window).width() <= 335) {
+            var textBlock = $('.hide'),
+                btn       = $('.hide-text'),
+                textHeight  = textBlock.innerHeight();
+
+            textBlock.css('height', 350 + 'px');
+
+            $(btn).click(function () {                              //text-main
+                if (textBlock.innerHeight() === 350) {
+                    textBlock.animate({ height: textHeight }, 500);
+                    $('.white-grain').css('display', 'none');
+                    $(this).text('Скрыть отзыв');
+                } else {
+                    textBlock.animate({ height: 350 }, 500);
+                    $('.white-grain').css('display', 'block');
+                    $(this).text('Развернуть отзыв');
+                }
+            });
+        }
     });
+
+    // product-phone ---------------------------------------------------------------------------------------------------
+    $(document).ready(function () {
+        $(".all-products__img .accord-header").click(function () {
+            if($(this).next("div").is(":visible")){
+                $(this).next("div").slideUp("slow");
+            }else{
+                $(".all-products__img .accord-content").slideUp("slow");
+
+                $(this).next("div").slideDown("slow");
+            }
+        });
+    });
+
 
 })(jQuery);
 
---
+++ success.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media-style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div id="wrapper" class="blue">
+
+    <div class="success">
+        <div class="container">
+            <div class="success-info">
+                <h1 class="title">Спасибо за вашу заявку. <br>  Стоимость стекла составляет <span class="gold">11 600руб.</span></h1>
+                <p class="text">В течение 1 рабочего дня с вами свяжется наш менеджер для <br> уточнения информации.</p>
+                <button class="btn">Вернуться на главную</button>
+            </div>
+            <div class="success-footer clearfix">
+                <div class="logo clearfix">
+                    <div class="logo-img">
+                        <a href="#"><img src="img/gold-logo.png" alt="logo"></a>
+                    </div>
+                    <div class="logo-info">
+                        <a class="logo-info__link" href="#">Frederiko.ru</a>
+                        <p class="logo-info__text">Скинали и фартуки для кухни</p>
+                    </div>
+                </div>
+                <address class="address clearfix">
+                    <div class="address__img">
+                        <img class="footer-img" src="img/white-placeholder.png" alt="">
+                    </div>
+                    <div class="address__text">
+                        <span class="text">г.Москва БЦ "Вега" <br> (ул. Вербная, д.8с1, оф.207)</span>
+                        <br>
+                        <a class="link" href="#">info@frederiko.ru</a>
+                    </div>
+                </address>
+                <div class="work-hours clearfix">
+                    <div class="work-hours__img">
+                        <img class="footer-img" src="img/white-clock.png" alt="">
+                    </div>
+                    <div class="work-hours__text">
+                        <span class="text">Мы работаем: <br> с 9 до 21 Без выходных</span>
+                    </div>
+                </div>
+                <div class="copyright-info">
+                    <a class="privacy-policy" href="#">Политика конфиденциальности</a>
+                    <p class="copyright">© "Frederiko.ru" 2014 - 2017. Все права защищены.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="slick/slick.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/script.js"></script>
+
+</div>
+
+</body>
+</html>
